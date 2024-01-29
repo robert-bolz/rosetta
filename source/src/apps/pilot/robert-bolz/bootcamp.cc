111c1abaa917bcf6cfa75409f7c1f8d6354aabde
robert-bolz <robert.mac.bolz@gmail.com>
2024-01-29 17:12:39 -0500

    Added in functionality to read in a PDB to robert-bolz/bootcamp

--- source/src/apps/pilot/robert-bolz/bootcamp.cc
+++ source/src/apps/pilot/robert-bolz/bootcamp.cc
@@ -8,9 +8,22 @@
 // // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
 #include <iostream>
+#include <basic/options/option.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <devel/init.hh> 
 
-int main() {
-	std::cout << "Hello World!!" << std::endl;
-	return 0;
+int main( int argc, char ** argv ) {
+
+devel::init( argc, argv );
+utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
+
+if ( filenames.size() > 0 ) {
+std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
 } 
+else {
+	std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
+	return 1;
+}
+}
+
 
