977686e4cc42c73b0d81fb7b46be498d25de676e
robert-bolz <robert.mac.bolz@gmail.com>
2024-01-30 10:59:30 -0500

    Added in a Queue Unit test in the test/protocols/bootcamp, and added in the Queue cc, hh files in src/protocols/bootcamp

---
+++ source/test/protocols/bootcamp/QueueTests.cxxtest.hh
@@ -0,0 +1,58 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file  protocols/bootcamp/QueueTests.cxxtest.hh
+/// @brief  Tests for the Queue class
+/// @author robert-bolz (robert.mac.bolz@gmail.com)
+
+
+// Test headers
+#include <test/UMoverTest.hh>
+#include <test/UTracer.hh>
+#include <cxxtest/TestSuite.h>
+#include <test/util/pose_funcs.hh>
+#include <test/core/init_util.hh>
+
+// Project Headers
+
+
+// Core Headers
+#include <core/pose/Pose.hh>
+#include <core/import_pose/import_pose.hh>
+
+// Utility, etc Headers
+#include <basic/Tracer.hh>
+
+static basic::Tracer TR("QueueTests");
+
+
+class QueueTests : public CxxTest::TestSuite {
+	//Define Variables
+
+public:
+
+	void setUp() {
+		core_init();
+
+	}
+
+	void tearDown() {
+
+	}
+
+
+
+	void test_first() {
+       TS_TRACE( "Running my first unit test!" );
+       TS_ASSERT( true );
+
+	}
+
+
+};
